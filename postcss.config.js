4950f74cd031c5a6b7153d8e37fe64370a144fa9
matteo97 <matteocracco97@gmail.com>
2025-12-19 13:09:45 +0100

    Added post-font-magician

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,10 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    'postcss-font-magician': {
+      foundries: ['custom'],
+      variants: true,
+      display: 'swap'
+    }
   }
 }
